1ca1f8212dc05a361b5717cb46edcff14338ee8f
Hcdubsy <15789441+hcw001@users.noreply.github.com>
2025-03-07 08:31:55 -0500

    Include Version Number in Hash

--- store.py
+++ store.py
@@ -12,6 +12,7 @@ PASSWORD = os.getenv("password")
 HOST = os.getenv("host")
 PORT = os.getenv("port")
 DBNAME = os.getenv("dbname")
+VERSION = os.getenv("version")
 
 def get_connection():
     try:
@@ -39,7 +40,7 @@ def logError(battle_id):
 
 def logOutputs(battle_id, outputs):
     try:
-        outputs_hash = JSONHash(outputs)
+        outputs_hash = JSONHash(f"{outputs}&v={VERSION}")
         connection = get_connection()
         cursor = connection.cursor()
         cursor.execute("INSERT INTO results (battle_id, is_error, hash, outputs) VALUES (%s, %s, %s, %s)", (battle_id, False, outputs_hash, outputs))
