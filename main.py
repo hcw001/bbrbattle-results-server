6a45462d3c5634eab6aa6f108ba448b927a69fc5
Hcdubsy <15789441+hcw001@users.noreply.github.com>
2025-03-19 08:13:20 -0500

    added user data to logging

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from simulation import Simulation
-from store import logError, logOutputs
+from store import logError, logOutputs, logUserRequest
 #from AccessHistory import fetchState, insertState
 
 app = Flask(__name__)
 
-#http://localhost:3000
+#CORS(app, origins='http://localhost:3000')
 CORS(app, origins='https://www.bbr40.com')
 
 #app.config['CORS_HEADERS'] = 'Content-Type'
@@ -14,7 +14,8 @@ CORS(app, origins='https://www.bbr40.com')
 #Add CORS headers
 @app.after_request
 def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = 'https://www.bbr40.com' #http://localhost:3000
+    #response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000' 
+    response.headers['Access-Control-Allow-Origin'] = 'https://www.bbr40.com'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
     return response
@@ -28,6 +29,8 @@ def getResults():
     try:
         # Parses JSON data from the request body
         data = request.get_json() 
+        #Store User Request
+        logUserRequest(battle_id=data.get('id'), request=request)
         # Stored as python dictionary
         terrain = data.get('terrain')
         attackerTech = data.get('attackerTech')
@@ -65,6 +68,7 @@ def getResults():
         return jsonify(response), 200,
     
     except Exception as e:
+        print('Error: ', e)
         logError(battle_id=data.get('id'))
         return {'code': 0, 'message': str(e), 'outputs': {}}, 200
 
--- store.py
+++ store.py
@@ -36,6 +36,7 @@ def logError(battle_id):
         cursor.close()
         connection.close()
     except Exception as e:
+        print("Log Error Failed.")
         return
 
 def logOutputs(battle_id, outputs):
@@ -48,4 +49,17 @@ def logOutputs(battle_id, outputs):
         cursor.close()
         connection.close()
     except Exception as e:
+        print("Log Outputs Failed.")
         return
+    
+def logUserRequest(battle_id, request):
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO users (battle_id, user_ip, user_agent, user_referer) VALUES (%s, %s, %s, %s)", (battle_id, request.remote_addr, request.headers.get('User-Agent'), request.headers.get('Referer')))
+        connection.commit()
+        connection.close()
+    except Exception as e:
+        print("Log User Failed")
+        return
+    
